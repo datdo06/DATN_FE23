b96878a689eef0cd4dbfb121eb84eae0f66380a7
bvh18112k3 <bvh18112003@gmail.com>
2023-11-17 10:48:11 +0700

    update

--- resources/views/mail/mail.blade.php
+++ resources/views/mail/mail.blade.php
@@ -46,12 +46,14 @@
         .header h1 {
             font-size: 24px;
         }
+
         .signature {
             font-family: 'Comic Sans MS', sans-serif;
             font-size: 18px;
             color: #333;
             font-weight: bold;
         }
+
         .contact-info {
             font-family: 'Arial', sans-serif;
             margin-top: 5px;
@@ -68,7 +70,8 @@
                 <h1>Email Hủy Đặt Homestay</h1>
             </div>
             <p><strong>Kính gửi quý khách {{$user->name}}</strong></p>
-            <p>Cảm ơn bạn đã sử dụng dịch vụ đặt homestay của chúng tôi. Chúng tôi rất tiếc phải thông báo rằng đơn đặt phòng của bạn đã bị hủy. Vui lòng xem chi tiết dưới đây:</p>
+            <p>Cảm ơn bạn đã sử dụng dịch vụ đặt homestay của chúng tôi. Chúng tôi rất tiếc phải thông báo rằng đơn đặt
+                phòng của bạn đã bị hủy. Vui lòng xem chi tiết dưới đây:</p>
 
             <table width="100%">
                 <tr>
@@ -76,13 +79,36 @@
                     <td><strong>{{$transaction->room->type->name}}</strong></td>
 
                 </tr>
+                @php
+                    $dayDifference = Helper::getDateDifference($transaction->check_in, $transaction->check_out);
+                    $price = ($transaction->room->price * $dayDifference);
+                @endphp
+
                 <tr>
-                    <td><strong>Ngày nhận phòng: {{\App\Helpers\Helper::dateFormat($transaction->check_in)}}</strong></td>
-                    <td><strong>Ngày trả phòng: {{\App\Helpers\Helper::dateFormat($transaction->check_out)}}</strong></td>
+                    <td><strong>Ngày nhận phòng: {{\App\Helpers\Helper::dateFormat($transaction->check_in)}}</strong>
+                    </td>
+                    <td><strong>Ngày trả phòng: {{\App\Helpers\Helper::dateFormat($transaction->check_out)}}</strong>
+                    </td>
                 </tr>
                 <tr>
-                    <th >Tiền bạn đã cọc: {{\App\Helpers\Helper::convertToRupiah($transaction->getTotalPayment())}}</th>
-                    <th >Tổng tiền: {{\App\Helpers\Helper::convertToRupiah($transaction->sum_money)}}</th>
+                    <td><strong>Tiền homestay: {{\App\Helpers\Helper::convertToRupiah($price)}}</strong></td>
+                    @if(!empty($transactionCoupon))
+                        @if($transactionCoupon->Coupon->coupon_condition == 2)
+                            <td><strong>Bạn được
+                                    giảm: {{\App\Helpers\Helper::convertToRupiah($transactionCoupon->Coupon->coupon_number)}}</strong>
+                            </td>
+                        @else
+                            <td><strong>Bạn được
+                                    giảm: {{\App\Helpers\Helper::convertToRupiah(($price*$transactionCoupon->Coupon->coupon_number/100))}}</strong>
+                            </td>
+                        @endif
+                    @endif
+                </tr>
+                <tr>
+                    <td><strong>Tiền bạn đã
+                            cọc: {{\App\Helpers\Helper::convertToRupiah($transaction->getTotalPayment())}}</strong></td>
+                    <td><strong>Tổng tiền: {{\App\Helpers\Helper::convertToRupiah($transaction->sum_money)}}</strong>
+                    </td>
                 </tr>
 
             </table>
@@ -90,23 +116,27 @@
                 @if($hoan == 0)
                     <div>
                         <p>Quý khách đã hủy đặt homestay trước 3 ngày nên sẽ được hoàn lại 100% phí đã đặt cọc</p>
-                        <p>Quý khách được hoàn {{\App\Helpers\Helper::convertToRupiah($transaction->getTotalPayment())}}</p>
+                        <p>Quý khách được
+                            hoàn {{\App\Helpers\Helper::convertToRupiah($transaction->getTotalPayment())}}</p>
                     </div>
                 @elseif($hoan == 15)
                     <div>
                         <p>Quý khách đã hủy đặt homestay từ 3 đến 7 ngày nên sẽ được hoàn lại 15% phí đã đặt cọc</p>
-                        <p>Quý khách được hoàn {{\App\Helpers\Helper::convertToRupiah($transaction->getTotalPayment() - ($transaction->getTotalPayment() * $hoan /100))}}</p>
+                        <p>Quý khách được
+                            hoàn {{\App\Helpers\Helper::convertToRupiah($transaction->getTotalPayment() - ($transaction->getTotalPayment() * $hoan /100))}}</p>
                     </div>
-                @elseif($hoan == 100){
+                @elseif($hoan == 100)
                     <div>
                         <div>
                             <p>Quý khách đã hủy đặt homestay sau 7 ngày nên sẽ bị mất 100% phí đã đặt cọc</p>
                             <p>Quý khách được hoàn {{\App\Helpers\Helper::convertToRupiah(0)}}</p>
                         </div>
                     </div>
+                @endif
             </div>
 
-            <p>Chúng tôi hiểu rằng có những thay đổi trong kế hoạch của bạn và việc hủy đặt homestay có thể gây phiền hà. Nếu bạn có bất kỳ câu hỏi hoặc cần hỗ trợ thêm, vui lòng liên hệ với chúng tôi.</p>
+            <p>Chúng tôi hiểu rằng có những thay đổi trong kế hoạch của bạn và việc hủy đặt homestay có thể gây phiền
+                hà. Nếu bạn có bất kỳ câu hỏi hoặc cần hỗ trợ thêm, vui lòng liên hệ với chúng tôi.</p>
             <p>Chúng tôi rất mong được phục vụ bạn trong tương lai và cảm ơn bạn đã lựa chọn dịch vụ của chúng tôi.</p>
 
             <p><strong>Trân trọng,</strong></p>
